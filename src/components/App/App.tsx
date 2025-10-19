0c171021b79ffb54f489b80a5d837d884d9cd2f8
XokinXelden <xokinxelden@gmail.com>
2025-10-19 17:28:10 +0300

    Роутинг немного изменён

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,33 @@
 import HeaderVac from "../Header/Header";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import SearchVacanciesPage from "../MainBlock/SearchVacanciesPage";
 import VacanciesPage from "../VacanciesPage/VacanciesPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/vacancies" element={<SearchVacanciesPage />} />
+      <Route path="vacancies/:id" element={<VacanciesPage />} />
+      <Route
+        path="*"
+        element={<Navigate to={"/vacancies"} replace={true} />} // NotFound
+      />
+    </>
+  )
+);
 
 function App() {
   return (
     <>
       <HeaderVac />
-      <Routes>
-        <Route path="/vacancies" element={<SearchVacanciesPage />} />
-        <Route path="vacancies/:id" element={<VacanciesPage />} />
-        <Route
-          path="*"
-          element={<Navigate to={"/vacancies"} replace={true} />} // NotFound
-        />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,13 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { store } from "./store.ts";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <MantineProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </MantineProvider>
-    </BrowserRouter>
+    <MantineProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MantineProvider>
   </StrictMode>
 );
