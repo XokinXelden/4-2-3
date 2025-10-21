86446da6464bd4c566f2c579dcb9124f37baee80
XokinXelden <xokinxelden@gmail.com>
2025-10-22 01:12:00 +0300

    ссылка "обо мне" теперь работает и в обратную сторону

--- src/components/Header/Panel.tsx
+++ src/components/Header/Panel.tsx
@@ -1,5 +1,5 @@
 import { Flex, Image, Text } from "@mantine/core";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useMatch, useNavigate } from "react-router-dom";
 
 function VacancyFE() {
   return (
@@ -21,11 +21,16 @@ function VacancyFE() {
   );
 }
 function AboutLink() {
+  const match = useMatch("/about");
+  const navigate = useNavigate();
   return (
     <Flex align="center" gap={5} variant="transparent" color="#00000073">
       <Image src="/user-circle.png" h={24} w={24} />
       <NavLink
-        to="/about"
+        to={match ? "#" : "/about"}
+        onClick={() => {
+          if (match) navigate(-1);
+        }}
         style={({ isActive }) => ({
           color: isActive ? "#4263EB" : "black",
           fontWeight: "normal",
